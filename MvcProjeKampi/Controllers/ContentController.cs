df2d6319339cbd614cbac96080a1454b170be99b
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2024-12-05 01:31:59 +0300

    ...

---
+++ MvcProjeKampi/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.EntityFramework;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        ContentManager cm = new ContentManager(new EfContentDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContentByHeading(int id)
+        {
+            var contenValues = cm.GetListByHeadingID(id);
+            return View(contenValues);
+        }
+    }
+}
